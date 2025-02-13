2a56c56b3e222e4f76722c66b5f1f05d91bf35f7
yuni <yuninam2128@gmail.com>
2025-02-14 05:35:47 +0900

    ✨ Feat 전진/멈춤 판별 및 카운트 기능 추가
    랜덤값을 활용해 전진/멈춤을 판별하고, 자동차별 전진 수를 카운트하는 기능을 추가하였다.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
 class Car {
-  constructor(name, count = 0) {
+  constructor(name,count = 0) {
     this.name = name;
     this.count = count;
   }
 }
 
 class Racing {
-  static AdvanceCount(Names){
+  static AdvanceCount(Names,Num){
     //전처리 
     let tempArray = [];
     let nameArray = [Names];
@@ -16,16 +16,35 @@ class Racing {
       tempArray = tempArray.concat(name.split(','));
     }
     nameArray = tempArray;
-    return nameArray;
+
+    //인스턴스화
+    let carInstances = nameArray.map(name => new Car(name));
+    
+    //전진 카운트 
+    for (let i = 0; i < Num; i++){
+      Console.print(`i:${i}`);
+        for (let j = 0; j < carInstances.length; j++){
+          const randomNum = Random.pickNumberInRange(0, 9);
+          const advance = ( randomNum >= 4) ? true : false;
+          if (advance){
+            carInstances[j].count++;
+            Console.print(`${carInstances[j].name}:${carInstances[j].count}`);
+          }
+        }
+    }
+    return carInstances;
   }
 }
+
+
 
 class App {
   //입력 받기 
   async run() {
     const inputName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ");
     const inputNum = await Console.readLineAsync("시도할 횟수는 몇 회인가요?: ");
-    Console.print(Racing.AdvanceCount(inputName));
+
+    Console.print(Racing.AdvanceCount(inputName,inputNum));
   }
 }
 
