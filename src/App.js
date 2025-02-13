413418ce18725981a6e46051af091424061ac2e7
yuni <yuninam2128@gmail.com>
2025-02-14 03:51:22 +0900

    ✨ Feat 입력 기능 추가
    문자열과 숫자를 입력받는 기능을 추가하였다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Console, Random } from "@woowacourse/mission-utils";
+
+class Car {
+  constructor(name, count) {
+    this.name = name;
+    this.count = count;
+  }
+}
+
 class App {
-  async run() {}
+  async run() {
+    const inputName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ");
+    const inputNum = await Console.readLineAsync("시도할 횟수는 몇 회인가요?: ");
+  }
 }
 
 export default App;
