bf169b914282d869a9b4c11637b7d64d54dac619
yuni <yuninam2128@gmail.com>
2025-02-14 04:17:21 +0900

    ✨ Feat 문자열 전처리 기능 추가
    쉼표를 기준으로 이름을 구분하는 문자열 전처리 기능을 추가하였다.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
 class Car {
-  constructor(name, count) {
+  constructor(name, count = 0) {
     this.name = name;
     this.count = count;
   }
 }
 
+class Racing {
+  static AdvanceCount(Names){
+    //전처리 
+    let tempArray = [];
+    let nameArray = [Names];
+    for (const name of nameArray) {
+      tempArray = tempArray.concat(name.split(','));
+    }
+    nameArray = tempArray;
+    return nameArray;
+  }
+}
+
 class App {
+  //입력 받기 
   async run() {
     const inputName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ");
     const inputNum = await Console.readLineAsync("시도할 횟수는 몇 회인가요?: ");
+    Console.print(Racing.AdvanceCount(inputName));
   }
 }
 
