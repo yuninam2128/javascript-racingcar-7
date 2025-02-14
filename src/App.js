ff72716c7552ac44b55542c0939271ac561397ce
yuni <yuninam2128@gmail.com>
2025-02-14 17:56:22 +0900

    ♻️ Refactor 중복 코드 제거 및 코드 구조 개선
    중복 코드를 함수로 만드는 등 코드를 간결하게 개선하였다.

--- src/App.js
+++ src/App.js
@@ -1,64 +1,55 @@
 import { Console, Random } from "@woowacourse/mission-utils";
 
 class Car {
-  constructor(name,count = 0) {
+  constructor(name) {
     this.name = name;
-    this.count = count;
+    this.count = 0;
+  }
+
+  // 전진 조건을 적용하는 메서드 
+  move() {
+    if (Random.pickNumberInRange(0, 9) >= 4) {
+      this.count++;
+    }
+  }
+
+  // 자동차 상태 출력 
+  print() {
+    Console.print(`${this.name} : ${'-'.repeat(this.count)}`);
   }
 }
 
 class Racing {
-
   constructor() {
     this.carInstances = [];
   }
 
   validateCarNames(names) {
-    // 전처리
-    let nameArray = names.split(",").map(name => name.trim()); 
-    
-    // 빈 문자열 처리 
-    nameArray = nameArray.filter(name => name !== "");
-
-    // 자동차 이름 중복 처리
-    nameArray = Array.from(new Set(nameArray));
-
-    // 자동차 이름은 5자 이하
-    nameArray.forEach(name => {
-      if (name.length > 5) {
-        throw new Error("[ERROR] IllegalArgumentException");
-      }
-    });
-
-    return nameArray;
+    // 쉼표 기준 분리 | 공백 처리 | 5자 이하 처리 | 중복 처리 
+    return [...new Set(names.split(",")
+      .map(name => name.trim())
+      .filter(name => name && name.length <= 5))];
   }
 
-  AdvanceCount(nameArray,Num){
-    //인스턴스화
+  advanceCount(nameArray,Num){
+    // 인스턴스화
     this.carInstances = nameArray.map(name => new Car(name));
 
-    //전진 카운트 
+    // 전진 카운트 
     for (let i = 0; i < Num; i++){
       Console.print('');
-        for (let j = 0; j < this.carInstances.length; j++){
-          const randomNum = Random.pickNumberInRange(0, 9);
-          const advance = ( randomNum >= 4) ? true : false;
-          if (advance){
-            this.carInstances[j].count++;
-          }
-          Console.print(`${this.carInstances[j].name} : ${'-'.repeat(this.carInstances[j].count)}`);
-        }
+      this.carInstances.forEach(car => {
+        car.move();
+        car.print();
+      })
     }
-    return this.carInstances;
   }
 
-  winner(){
-    //가장 큰 값 찾기
-    const maxValue = Math.max(...this.carInstances.map(itemm => itemm.count)); 
-    //가장 큰 값과 같은 값을 가진 CarName 만 추출
-    const maxCarNames = this.carInstances.filter(item => item.count === maxValue).map(item => item.name); 
-    
-    return maxCarNames;
+  getWinner(){
+    // 가장 큰 값 찾기
+    const maxCount = Math.max(...this.carInstances.map(car => car.count));
+    // 가장 큰 값과 같은 값을 가진 CarName 만 추출
+    return this.carInstances.filter(car => car.count === maxCount).map(car => car.name);
   }
 }
 
@@ -67,20 +58,20 @@ class App {
     try{
       const racing = new Racing();
 
-      //이름 입력받기, 예외 처리 
+      // 이름 입력받기, 예외 처리 
       const inputName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ");
       const nameArray = racing.validateCarNames(inputName);
 
-      //시도 횟수 입력받기, 예외 처리
+      // 시도 횟수 입력받기, 예외 처리
       const inputNum = await Console.readLineAsync("시도할 횟수는 몇 회인가요?: "); 
       if (isNaN(inputNum) || inputNum <= 0) {
         throw new Error("[ERROR] IllegalArgumentException");
       }
 
-      //출력
+      // 출력
       Console.print('실행 결과');
-      racing.AdvanceCount(nameArray,inputNum);
-      Console.print(`최종 우승자 : ${racing.winner().join(', ')}`);
+      racing.advanceCount(nameArray,inputNum);
+      Console.print(`최종 우승자 : ${racing.getWinner().join(', ')}`);
   
     } catch (error) {
       Console.print(error.message);
