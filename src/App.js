7dd7c239e3f4569f02903ea9087b3993b8f40c2a
yuni <yuninam2128@gmail.com>
2025-02-14 17:25:33 +0900

    ✨ Feat 예외 처리 추가
    중복, 조건에 어긋나는 입력, 빈 문자열을 처리하는 기능을 추가하였다.

--- src/App.js
+++ src/App.js
@@ -8,19 +8,32 @@ class Car {
 }
 
 class Racing {
+
   constructor() {
     this.carInstances = [];
   }
 
-  static AdvanceCount(Names,Num){
-    //전처리 
-    let tempArray = [];
-    let nameArray = [Names];
-    for (const name of nameArray) {
-      tempArray = tempArray.concat(name.split(','));
-    }
-    nameArray = tempArray;
+  validateCarNames(names) {
+    // 전처리
+    let nameArray = names.split(",").map(name => name.trim()); 
+    
+    // 빈 문자열 처리 
+    nameArray = nameArray.filter(name => name !== "");
 
+    // 자동차 이름 중복 처리
+    nameArray = Array.from(new Set(nameArray));
+
+    // 자동차 이름은 5자 이하
+    nameArray.forEach(name => {
+      if (name.length > 5) {
+        throw new Error("[ERROR] IllegalArgumentException");
+      }
+    });
+
+    return nameArray;
+  }
+
+  AdvanceCount(nameArray,Num){
     //인스턴스화
     this.carInstances = nameArray.map(name => new Car(name));
 
@@ -39,7 +52,7 @@ class Racing {
     return this.carInstances;
   }
 
-  static winner(){
+  winner(){
     //가장 큰 값 찾기
     const maxValue = Math.max(...this.carInstances.map(itemm => itemm.count)); 
     //가장 큰 값과 같은 값을 가진 CarName 만 추출
@@ -49,17 +62,30 @@ class Racing {
   }
 }
 
-
 class App {
   async run() {
-    //입력 받기 
-    const inputName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ");
-    const inputNum = await Console.readLineAsync("시도할 횟수는 몇 회인가요?: ");
+    try{
+      const racing = new Racing();
 
-    //출력
-    Console.print('실행 결과');
-    Racing.AdvanceCount(inputName,inputNum);
-    Console.print(`최종 우승자 : ${Racing.winner().join(', ')}`);
+      //이름 입력받기, 예외 처리 
+      const inputName = await Console.readLineAsync("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ");
+      const nameArray = racing.validateCarNames(inputName);
+
+      //시도 횟수 입력받기, 예외 처리
+      const inputNum = await Console.readLineAsync("시도할 횟수는 몇 회인가요?: "); 
+      if (isNaN(inputNum) || inputNum <= 0) {
+        throw new Error("[ERROR] IllegalArgumentException");
+      }
+
+      //출력
+      Console.print('실행 결과');
+      racing.AdvanceCount(nameArray,inputNum);
+      Console.print(`최종 우승자 : ${racing.winner().join(', ')}`);
+  
+    } catch (error) {
+      Console.print(error.message);
+      return Promise.reject(error);
+    }
   }
 }
 
